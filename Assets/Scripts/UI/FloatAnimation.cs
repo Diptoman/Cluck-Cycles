7b0640a38e0319f1112b8504c8fd60ce72b17020
Pranjal Bisht <pranjalnitrox@gmail.com>
2025-08-02 21:24:40 +0530

    Upgraded rotator to general sine-based animation

--- Assets/Scripts/UI/FloatAnimation.cs
+++ Assets/Scripts/UI/FloatAnimation.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
 
-public class FloatRotator : MonoBehaviour
+public class FloatAnimation : MonoBehaviour
 {
     private float randomStart;
     private Vector3 startRot;
+    private Vector3 startPos;
     public Vector3 rotSpeed;
     public Vector3 rotAmount;
+
+    public Vector3 posSpeed;
+    public Vector3 posAmount;
+
     public bool isActive { get; private set; }
 
     // Start is called before the first frame update
     void Start()
     {
         startRot = transform.localEulerAngles;
+        startPos = transform.localPosition;
         randomStart = Random.Range(0f, 5f);
     }
 
@@ -33,5 +39,10 @@ public class FloatRotator : MonoBehaviour
 
         var finalAngles = startRot + new Vector3(x, y, z);
         transform.localEulerAngles = finalAngles;
+
+        var posX = Mathf.Sin(posSpeed.x * t) * posAmount.x;
+        var posY = Mathf.Sin(posSpeed.x * t) * posAmount.y;
+        var posZ = Mathf.Sin(posSpeed.x * t) * posAmount.z;
+        transform.localPosition = startPos + new Vector3(posX, posY, posZ);
     }
 }
